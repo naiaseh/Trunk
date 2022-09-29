7c4731c617ef9d12633ce06e0cd200c3d1b5da42
naiaseh <40075965+naiaseh@users.noreply.github.com>
2022-09-29 16:24:46 -0400

    Add files via upload
    Added Job.py

---
+++ Job.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 11:45:20 2022
+
+@author: nadiaaiaseh
+"""
+import numpy as np
+#import matplotlib.pyplot as plt
+
+x = np.linspace(0,np.pi*2,100)
+y = np.cos(x)
+
+print(x[0:3])
+# plt.plot(x,y)
+#plt.show()
