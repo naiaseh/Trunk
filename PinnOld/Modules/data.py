742e65e8df30c39427469b30f55e5c1106965684
Nadia Aiaseh <naiaseh@uwo.ca>
2023-01-29 19:15:26 -0500

    rough implementation for travelling (time-indep) Kawahara

--- PinnOld/Modules/data.py
+++ PinnOld/Modules/data.py
@@ -12,6 +12,28 @@
 
 import numpy as np
 import tensorflow as tf
+from scipy.optimize import fsolve
+
+def kawaharaCosEqnsPos(U, a1, alpha, beta, sigma, N): #defining the equation
+    kawaharaCosEqnsPos=np.zeros(N+2,dtype='float64') #first set all the equations equal to 0=0
+    
+    
+    V=U[0] #Vector U contains the unknown coefficients and the unknown speed, we have N+2 unknowns
+    a=U[1::]
+    
+    ### for the coefficients ###
+    
+    for k in range(N+1):
+        sum1=0.#set the sums for the nonlinear term equal to 0 when solving for every coefficient
+        sum2=0.
+        for n in range(k,N+1):
+            sum1=sum1+a[n]*a[n-k] 
+        for n in range(0,k):
+            sum2=sum2+a[n]*a[k-n] 
+        kawaharaCosEqnsPos[k]=((V*a[k] + 1./2.*sigma*sum1 + 1./2.*sigma*sum2 - alpha*k**2*a[k] + beta*k**4*a[k]))
+        
+    kawaharaCosEqnsPos[N+1]=-a1+a[1] #for the last equation, linearize to obtain an equation for speed
+    return kawaharaCosEqnsPos
 
 def simulate_kdv(n_samples, phi_function, boundary_function, length, time, random_seed = 42, dtype=tf.float32) -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
     """
@@ -50,6 +72,96 @@ def simulate_kdv(n_samples, phi_function, boundary_function, length, time, rando
     y_eqn = tf.zeros((n_samples, 1))
     y_phi = phi_function(tx_init)
     y_boundary = boundary_function(tx_boundary)
+
+    # y_eqn = tf.convert_to_tensor(y_eqn, dtype = dtype)
+    # y_phi = tf.convert_to_tensor(y_phi, dtype = dtype)
+    # y_boundary = tf.convert_to_tensor(y_boundary, dtype = dtype)
+
+    return (tx_eqn, y_eqn), (tx_init, y_phi), (tx_boundary, y_boundary)
+
+def simulate_travelling_kawahara(n_samples, length, time, random_seed = 42, dtype=tf.float32) -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
+    """
+    Simulate the heat equation in 1D with a given initial condition and Dirichlet boundary conditions.
+    Args:
+        n_samples (int): number of samples to generate
+        phi_function (function): Function that returns the initial condition of the heat equation on u.
+        boundary_function (function): Function that returns the boundary condition of the heat equation on u.
+        length (float): Length of the domain.
+        time (float): Time frame of the simulation.
+        random_seed (int, optional): Random seed for reproducibility. Defaults to 42.
+        dtype (tf.dtype, optional): Data type of the samples. Defaults to tf.float32.
+
+    Returns:
+        tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]: Samples of the heat equation. Returns a tuple of tensors (equation_samples, initial_samples, boundary_samples).
+    """
+
+    r = np.random.RandomState(random_seed)
+    t = r.uniform(0, time, (n_samples, 1))
+    x = r.uniform(0, length, (n_samples, 1))
+    tx_eqn = np.concatenate((t, x), axis = 1)
+
+    t_init = np.zeros((n_samples, 1))
+    x_init = r.uniform(0, length, (n_samples, 1))
+    tx_init = np.concatenate((t_init, x_init), axis = 1)
+
+    t_boundary = r.uniform(0, time, (n_samples, 1))
+    x_boundary = np.ones((n_samples//2, 1))*length
+    x_boundary = np.append(x_boundary, np.zeros((n_samples - n_samples//2, 1)), axis=0)
+    tx_boundary = np.concatenate((t_boundary, x_boundary), axis = 1)
+
+    tx_eqn = tf.convert_to_tensor(tx_eqn, dtype = dtype)
+    tx_init = tf.convert_to_tensor(tx_init, dtype = dtype)
+    tx_boundary = tf.convert_to_tensor(tx_boundary, dtype = dtype)
+
+    thirdAlpha = 1.
+    fifthBeta =1/4
+    nonlinSigma = 1.
+    c = thirdAlpha - fifthBeta
+    # L = (np.pi)*2
+    # spacext = tx[:, 1:2]
+    conSteps =1500 # number of continuation steps
+    a1=1.0e-6 # beginning amplitude
+    aF=1.0e-2 # ending amplitude
+    aS = np.linspace(a1,aF,conSteps) # vector of free parameter a1 (amplitudes)
+    velocities=np.zeros(conSteps) # Tracks all the velocities for bifurcation branch
+    NN=21 # number of modes at which the Fourier series is truncated 
+    uguess = np.zeros(NN+2)
+
+    uguess[0] = c # uguess is our initial guess vector, it has zeros everywhere, except for first two elements: c and a1
+    uguess[1] = a1
+    V=c
+    for k in range(conSteps):
+
+
+        solution = fsolve(kawaharaCosEqnsPos, uguess, args=(aS[k], thirdAlpha, fifthBeta, nonlinSigma, NN),xtol=1.e-8) #notice amplitude changes with every iteration
+        soln = solution[1::] #all the As (excludes speed)
+
+        V = solution[0]
+        uguess = np.concatenate((V,solution[1],aS[k],solution[3::]),axis=None) #update initial guess
+        
+
+    # generating the solution in real space made of cosines
+        # phi = soln[0]*np.cos(0.*(tx[:, 1:2]-V*tx[:, 0:1]))
+        phi_init = soln[0]*np.cos(0.*(tx_init[:, 1:2]-V*tx_init[:, 0:1]))
+        phi_boundary = soln[0]*np.cos(0.*(tx_boundary[:, 1:2]-V*tx_boundary[:, 0:1]))
+        # phix = -0.*soln[0]*np.sin(0.*(tx[:, 1:2]-V*tx[:, 0:1]))
+        ii = 0.
+        for aii in soln[1:]:
+            ii = ii+1.
+            # phi = phi + aii*np.cos(ii*(tx[:, 1:2]-V*tx[:, 0:1]))
+            phi_init = phi_init + aii*np.cos(ii*(tx_init[:, 1:2]-V*tx_init[:, 0:1]))
+            phi_boundary = phi_boundary + aii*np.cos(ii*(tx_boundary[:, 1:2]-V*tx_boundary[:, 0:1]))
+            # phix = phix - (ii)*aii*np.sin(ii*(tx[:, 1:2]-V*tx[:, 0:1]))
+
+
+
+
+
+        velocities[k]=solution[0]
+
+    y_eqn = tf.zeros((n_samples, 1))
+    y_phi = phi_init
+    y_boundary = phi_boundary
 
     # y_eqn = tf.convert_to_tensor(y_eqn, dtype = dtype)
     # y_phi = tf.convert_to_tensor(y_phi, dtype = dtype)
--- PinnOld/Modules/models.py
+++ PinnOld/Modules/models.py
@@ -189,3 +189,161 @@ class KdvPinn(tf.keras.Model):
     outputs = tf.keras.layers.Dense(n_outputs, kernel_initializer=initialization)(x)
     return tf.keras.Model(inputs=[inputs], outputs = [outputs])
 
+
+class travelKawaharaPinn(tf.keras.Model):
+  """
+  Keras PINN model for the Heat PDE.
+  Attributes:
+    network (keras.Model): The neural network used to approximate the solution.
+
+  """
+
+  def __init__(self, network: "tf.keras.Model", alpha: float = 1.0,beta: float = 1.0/4.0,sigma: float = 2.0) -> None:
+    """
+    Args:
+      network: A keras model representing the backbone neural network.
+      alpha, beta, sigma: PDE params
+    """
+    super().__init__()
+    self.network = network
+    self.sigma = sigma
+    self.alpha = alpha
+    self.beta = beta
+
+
+  def fit(self, inputs, labels, epochs, optimizer, u_exact=None, progress_interval=500) -> dict[str, list[float]]:
+    """
+    Train the model with the given inputs and optimizer.
+
+    Args:
+      inputs: A list of tensors, where the first tensor is the equation data,
+        the second tensor is the initial condition data, and the third tensor is the
+        boundary condition data.
+      labels: A list of tensors, where the first tensor is the phi initial condition labels,
+        the second tensor is the boundary condition labels.
+      epochs: The number of epochs to train for.
+      optimizer : The optimizer to use for training.
+      progress_interval: The number of epochs between each progress report.
+    Returns:
+      A dictionary containing the loss history for each of the three loss terms.
+    """
+    history = _create_history_dict()
+
+    start_time = time.time()
+    for epoch in range(epochs):
+      with tf.GradientTape() as tape:
+        u, residual, u_init, u_bndry = self.call(inputs) #question residual, not u right?
+
+        loss_residual = tf.reduce_mean(tf.square(residual))
+        loss_init = tf.reduce_mean(tf.square(u_init - labels[0]))
+        loss_boundary = tf.reduce_mean(tf.square(u_bndry - labels[1]))
+        loss = loss_residual + loss_init + loss_boundary
+
+      grads = tape.gradient(loss, self.trainable_weights)
+      optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+      if u_exact is not None:
+        abs_error = tf.reduce_mean(tf.abs(u - u_exact))
+      else:
+        abs_error = None
+
+      _add_to_history_dict(history, loss_residual, loss_init, loss_boundary, abs_error)
+      
+      if epoch % progress_interval == 0:
+        print(f"Epoch: {epoch} Loss: {loss.numpy():.4f} Total Elapsed Time: {time.time() - start_time:.2f}")
+    
+    return history
+
+  
+  @tf.function
+  def input_gradient(self, tx):
+    """
+    Compute derivatives
+
+    Args:
+      tx: input tensor of shape (n_inputs, 2)
+
+    Returns:
+      u_t, u_3x, u_5x, u
+    """
+    with tf.GradientTape() as g5:
+      g5.watch(tx)
+      with tf.GradientTape() as g4:
+        g4.watch(tx)
+        with tf.GradientTape() as ggg:
+            ggg.watch(tx)
+            with tf.GradientTape() as gg:
+                gg.watch(tx)
+                with tf.GradientTape() as g:
+                    g.watch(tx)
+                    u = self.network(tx)
+
+                first_order = g.batch_jacobian(u, tx)
+                du_dt = first_order[..., 0]
+                du_dx = first_order[..., 1]
+
+            d2u_dx2 = gg.batch_jacobian(du_dx, tx)[..., 1]
+        d3u_dx3 = ggg.batch_jacobian(d2u_dx2, tx)[..., 1]
+      d4u_dx4 = g4.batch_jacobian(d3u_dx3, tx)[..., 1]
+    d5u_dx5 = g5.batch_jacobian(d4u_dx4, tx)[..., 1]
+
+    return u, du_dt, du_dx, d3u_dx3, d5u_dx5
+    
+
+  
+  def call(self, inputs):
+    """
+    Performs forward pass of the model, computing the PDE residual and the initial and boundary conditions.
+
+    Args:
+      inputs: A list of tensors, where the first tensor is the equation data,
+        the second tensor is the initial condition data, and the third tensor is the
+        boundary condition data.
+
+    Returns:
+        pde_residual: The PDE residual of shape (n_inputs, 1)
+        u_init: The initial condition output of shape (n_inputs, 1)
+        u_bndry: The boundary condition output of shape (n_inputs, 1)
+
+    """
+
+    tx_equation = inputs[0]
+    tx_init = inputs[1]
+    tx_bound = inputs[2]
+
+    u, du_dt, du_dx, d3u_dx3, d5u_dx5 = self.input_gradient(tx_equation)
+
+    
+    # Calculate PDE residual
+    pde_residual = du_dt - self.alpha * d3u_dx3 + (self.beta) * d5u_dx5 * self.sigma * u * du_dx
+
+    n_i = tf.shape(tx_init)[0]
+    tx_ib = tf.concat([tx_init, tx_bound], axis=0)
+    u_ib = self.network(tx_ib)
+    u_init = u_ib[:n_i]
+    u_bndry = u_ib[n_i:]
+
+    return u, pde_residual, u_init, u_bndry
+
+  @staticmethod
+  def build_network(layers, n_inputs=2, n_outputs=1, activation=tf.keras.activations.tanh, initialization=tf.keras.initializers.glorot_normal):
+    """
+    Builds a fully connected neural network with the specified number of layers and nodes per layer.
+
+    Args:
+        layers (list): List of integers specifying the number of nodes in each layer.
+        n_inputs (int): Number of inputs to the network.
+        n_outputs (int): Number of outputs from the network.
+        activation (function): Activation function to use in each layer.
+        initialization (function): Initialization function to use in each layer.
+    returns:
+        keras.Model: A keras model representing the neural network.
+    """
+    inputs = tf.keras.layers.Input((n_inputs))
+    x = inputs
+    for i in layers:
+      x = tf.keras.layers.Dense(i, activation = activation, kernel_initializer=initialization)(x)
+    
+    outputs = tf.keras.layers.Dense(n_outputs, kernel_initializer=initialization)(x)
+    return tf.keras.Model(inputs=[inputs], outputs = [outputs])
+
