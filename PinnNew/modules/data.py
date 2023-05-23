b0c4648b2f8846a6b0918b914481615aad7e3f73
Nadia Aiaseh <naiaseh@uwo.ca>
2023-05-23 14:56:00 -0400

    implementation for travelling wave solution (Stokes waves) to Kawahara

--- PinnNew/modules/data.py
+++ PinnNew/modules/data.py
@@ -4,6 +4,8 @@ Module for the data generation of the heat, wave, schrodinger, burgers, and pois
 import tensorflow as tf
 from typing import Callable, Tuple
 import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
 
 
 def simulate_burgers(n_samples, init_function=None, boundary_function=None, n_init=None, n_bndry=None, random_seed=42, dtype=tf.float32) \
@@ -520,4 +522,102 @@ def simulate_transport(n_samples, n_init, n_boundary, solver_function, beta, u0=
     if return_mesh:
         return (tx_samples, u_samples, samples_residuals), (tx_init, u_init), (tx_bnd_start, tx_bnd_end, u_boundary), (X, T, U)
     return (tx_samples, u_samples, samples_residuals), (tx_init, u_init), (tx_bnd_start, tx_bnd_end, u_boundary)
+
+def kawaharaCosEqnsPos(U, a1, alpha, beta, sigma, N): 
+    kawaharaCosEqnsPos = np.zeros(N+2,dtype='float64')
+    V=U[0] # Vector U contains the unknown coefficients and the unknown speed, we have N+2 unknowns
+    a=U[1::]
+    ### for the coefficients ###
+    for k in range(N+1):
+        sum1=0.#set the sums for the nonlinear term equal to 0 when solving for every coefficient
+        sum2=0.
+        for n in range(k,N+1):
+            sum1=sum1+a[n]*a[n-k] 
+        for n in range(0,k):
+            sum2=sum2+a[n]*a[k-n] 
+        kawaharaCosEqnsPos[k]=((V*a[k] + 1./2.*sigma*sum1 + 1./2.*sigma*sum2 - alpha*k**2*a[k] + beta*k**4*a[k]))
+        
+    kawaharaCosEqnsPos[N+1] = -a1 + a[1] #for the last equation, linearize to obtain an equation for speed
+    return kawaharaCosEqnsPos
+
+def simulate_travel_kawahara(n_samples, x_start, length, time, random_seed = 42, dtype=tf.float32) -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
+    """
+    Simulate the Kawahara equation in 1D with a given.
+    Args:
+        n_samples (int): number of samples to generate
+        x_start (float): start of the domain.
+        length (float): end of the domain.
+        time (float): Time frame of the simulation.
+        random_seed (int, optional): Random seed for reproducibility. Defaults to 42.
+        dtype (tf.dtype, optional): Data type of the samples. Defaults to tf.float32.
+
+    Returns:
+        tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]: Samples of the heat equation. Returns a tuple of tensors (equation_samples, initial_samples, boundary_samples).
+    """
+
+  
+
+    
+    t = tf.random.uniform((n_samples, 1), 0, time, dtype=dtype, seed=random_seed)
+    x = tf.random.uniform((n_samples, 1), x_start, length, dtype=dtype, seed=random_seed)
+    tx_eqn = tf.concat([t, x], axis=1)
+    
+    t_init = tf.zeros((n_samples, 1), dtype=dtype)
+    x_init = tf.random.uniform((n_samples, 1), x_start, length, seed=random_seed, dtype=dtype)
+    tx_init = tf.concat([t_init, x_init], axis=1)
+    # y_init = init_function(tx_init)
+
+    t_boundary = tf.random.uniform((n_samples, 1), 0, time, dtype=dtype, seed=random_seed)
+    bnd_start = tf.ones((n_samples//2, 1), dtype=dtype)*x_start
+    bnd_end = tf.ones((n_samples//2, 1), dtype=dtype)*length
+    x_bnd = tf.concat([bnd_start, bnd_end], axis=0)
+
+    tx_boundary = tf.concat([t_boundary, x_bnd], axis=1)
+    tx_boundary = tf.random.shuffle(tx_boundary, seed=random_seed)
+    # y_boundary = boundary_function(tx_boundary)
+
+    thirdAlpha = 1.
+    fifthBeta = 1/4
+    nonlinSigma = 1.
+    c = thirdAlpha - fifthBeta
+    conSteps = 1500 # number of continuation steps
+    a1 = 1.0e-6 # beginning amplitude
+    aF = 0.001 # ending amplitude
+    aS = np.linspace(a1,aF,conSteps) # vector of free parameter a1 (amplitudes)
+    velocities = np.zeros(conSteps) # Tracks all the velocities for bifurcation branch
+    NN = 21 # number of modes at which the Fourier series is truncated 
+    uguess = np.zeros(NN+2)
+
+    uguess[0] = c # uguess is our initial guess vector, it has zeros everywhere, except for first two elements: c and a1
+    uguess[1] = a1
+    V = c
+    for k in range(conSteps):
+        solution = fsolve(kawaharaCosEqnsPos, uguess, args=(aS[k], thirdAlpha, fifthBeta, nonlinSigma, NN),xtol=1.e-8) 
+        soln = solution[1::] # all the As (excludes speed)
+        V = solution[0]
+        uguess = np.concatenate((V,solution[1],aS[k],solution[3::]),axis=None) #update initial guess
+
+        phi_init = soln[0]*np.cos(0.*(tx_init[:, 1:2]-0*tx_init[:, 0:1]))
+        phi_boundary = soln[0]*np.cos(0.*(tx_boundary[:, 1:2]-0*tx_boundary[:, 0:1]))
+        u_exact = soln[0]*np.cos(0.*(tx_init[:, 1:2])-0*tx_eqn[:, 0:1])
+        velocities[k] = solution[0]
+        ii = 0.
+    for aii in soln[1:]:
+        ii = ii+1.
+        phi_init = phi_init + aii*np.cos(ii*(tx_init[:, 1:2]-0*tx_init[:, 0:1]))
+        phi_boundary = phi_boundary + aii*np.cos(ii*(tx_boundary[:, 1:2]-0*tx_boundary[:, 0:1]))
+        u_exact = u_exact + aii*np.cos(ii*(tx_eqn[:, 1:2]-0*tx_eqn[:, 0:1])) 
+    plt.plot(tx_eqn[:,1:2],u_exact,'.')
+    plt.plot(tx_boundary[:,1:2],phi_boundary,'.')
+    plt.show()
+
+    y_eqn = tf.zeros((n_samples, 1))
+    y_phi = phi_init
+    y_boundary = phi_boundary
+
+    # y_eqn = tf.convert_to_tensor(y_eqn, dtype = dtype)
+    # y_phi = tf.convert_to_tensor(y_phi, dtype = dtype)
+    # y_boundary = tf.convert_to_tensor(y_boundary, dtype = dtype)
+
+    return (tx_eqn, y_eqn, u_exact), (tx_init, y_phi), (tx_boundary, y_boundary), solution
 
